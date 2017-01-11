e4a3b59496a2a5cc2ae240a2cdc6b5b07e53f55e
liwei <liwei5917@gmail.com>
2017-01-11 10:36:23 +0800

    implement step4 of the step5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,13 +1,14 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.order("created_at DESC")
   end
 
   def new
